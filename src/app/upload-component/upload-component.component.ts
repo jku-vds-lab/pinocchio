1dfa0a9012f8b18f5eb6c33a5af23e4ca8d57182
gregorst99 <gregor.stadler1@gmx.at>
2023-02-10 12:30:05 +0100

    new feature nummerization

--- src/app/upload-component/upload-component.component.ts
+++ src/app/upload-component/upload-component.component.ts
@@ -53,4 +53,63 @@ export class UploadComponentComponent implements OnInit {
   }
 
 
+  convert() {
+    let nx = GlobalVars.datahandler.categoricColumns
+    for(let i=0;i<nx.length;i++){
+     console.log(nx[i].value)
+      this.convert_and_extend(nx[i].value);
+    }
+
+  }
+
+  private convert_and_extend(value:number) {
+    /*console.log(GlobalVars.datahandler.table)
+    console.log(GlobalVars.datahandler.numericColumns)
+    console.log(GlobalVars.datahandler.columnNames)*/
+
+    //get num of individual values
+    let myset: string[] = [];
+    for(let j=0;j<GlobalVars.datahandler.table.length;j++){
+
+      let tv = GlobalVars.datahandler.table[j][value]
+      if(myset.indexOf(tv)<0){
+        myset.push(tv)
+        GlobalVars.datahandler.columnNames.push(GlobalVars.datahandler.columnNames[value]+"#"+tv)
+        GlobalVars.datahandler.numericColumns.push({key: GlobalVars.datahandler.columnNames[value]+"#"+tv, value: j})
+        GlobalVars.datahandler.dataType.push("continuous")
+      }
+
+
+    }
+
+    console.log(myset)
+
+
+    //generate for each a column.
+
+
+
+    //iterate over rows
+    //go to column value
+    //copy
+    //push one additional column with our value
+
+    for(let i=0;i<GlobalVars.datahandler.table.length;i++){
+      for(let j=0;j<myset.length;j++){
+        if(myset[j] == GlobalVars.datahandler.table[i][value]){
+          GlobalVars.datahandler.table[i].push(1)
+        }
+        else {
+          GlobalVars.datahandler.table[i].push(0)
+
+        }
+
+      }
+    }
+
+    console.log(GlobalVars.datahandler.numericColumns)
+
+
+
+  }
 }
